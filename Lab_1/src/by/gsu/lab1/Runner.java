c037dcd5418cf59f0bed6e627b6f9c4b97f90d3e
SofiaAS37 <zaharowa.sofija2018@yandex.by>
2017-10-08 20:21:19 +0300

    minor changes

--- Lab_1/src/by/gsu/lab1/Runner.java
+++ Lab_1/src/by/gsu/lab1/Runner.java
@@ -9,11 +9,16 @@ public class Runner {
 				new Book("Product 3", 1780, true), 
 				new Book("Product 4", 1860, false), 
 				new Book("Product 5", 2300, true), 
+				new Book("Product 6", 2431, true),
+				new Book("Product 7", 3799, true),
+				new Book("Product 8", 2120, false),
+				new Book("Product 9", 1999, true),
+				new Book("Product 10", 2549, true)
 				}; 
 				for(int i=0; i<product.length; i++){ 
 				System.out.println(product[i]); 
 				} 
-				System.out.println("книг с илюстрациями: "+Book.getTotalkolvo()+"; Общая стоимосить: "+Book.getTotalPrice()); 
+				System.out.println("Книг с илюстрациями: "+Book.getTotalkolvo()+"; Общая стоимосить: "+Book.getTotalPrice()); 
 
 				} 
 
